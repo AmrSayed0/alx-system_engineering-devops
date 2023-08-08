9cf9f61b95f7ce55cf67cc2e3446295d182a5295
AmrSayed0 <amracademycode@gmail.com>
2023-08-08 19:49:50 +0300

    [Update] Fix error no. 4

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -14,8 +14,27 @@ def top_ten(subreddit):
     }
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    
+    if response.status_code == 200:
+        data = response.json().get("data")
+        if data and "children" in data:
+            children = data["children"]
+            if children:
+                [print(child.get("data").get("title")) for child in children]
+            else:
+                print("No posts found in the subreddit.")
+        else:
+            print("No valid data found in the response.")
+    elif response.status_code == 404:
+        print("Subreddit not found.")
+    else:
+        print("An error occurred. Status code:", response.status_code)
+
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
